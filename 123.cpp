afa2a572d1a6d5ddd3e13943532636d890e37def
Uliya755 <sergeichikyulya@mail.ru>
2019-10-27 18:01:29 +0500

    v6

--- 123.cpp
+++ 123.cpp
@@ -5,8 +5,16 @@ using namespace std;
 int main(){
    char str[100];
    bool IsBreak=false;
-   cout<<"Vvtdite"<<endl;
+   cout<<"Vvedite"<<endl;
    cin>>str;
-   cout<<"Hello world"<<str<<endl;
+   for (int i=0; str[i]!='\0'; i++)
+   if (str[i]<48 || str[i]>57) {
+      cout<<"Vy vveli ne chislo\n"; 
+      IsBreak=true; 
+      break;
+   }
+   if(IsBreak==false){
+      cout<<"Hello world"<<str<<"7"<<endl;
+   }
    return 0;
 }
